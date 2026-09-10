743f36ee2f95088a6c45a8642fe3200834942954
kanakagarwal0901 <kanakagarwal0901@gmail.com>
2026-09-11 00:35:15 +0530

    Typecasting in this

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -5,18 +5,18 @@ pragma solidity 0.8.18;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory{
-    SimpleStorage[] public listofSimpleStorages;
+    address[] public listofSimpleStoragesAddress;
     function CreateSimpleStorage() public{
         SimpleStorage mySimpleStorage = new SimpleStorage();
-        listofSimpleStorages.push(mySimpleStorage);
+        listofSimpleStoragesAddress.push(address(mySimpleStorage));
     }
     function sfStore(uint _simpleStorageIndex, uint num1) public{
-        SimpleStorage mySimpleStorage = listofSimpleStorages[_simpleStorageIndex];
-        mySimpleStorage.store(num1);
+        address mySimpleStorage = listofSimpleStoragesAddress[_simpleStorageIndex];
+        SimpleStorage(mySimpleStorage).store(num1);
     }
     function sfView(uint _simpleStorageIndex) public view returns (uint){
-        SimpleStorage mySimpleStorage = listofSimpleStorages[_simpleStorageIndex];
-        return mySimpleStorage.retrieve();
+        address mySimpleStorage = listofSimpleStoragesAddress[_simpleStorageIndex];
+        return SimpleStorage(mySimpleStorage).retrieve();
     }
 }
 
