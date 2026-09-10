e7549689300732925b9e428dfe45544fbefd144c
kanakagarwal0901 <kanakagarwal0901@gmail.com>
2026-09-11 00:25:56 +0530

    Storage Factory with more functions
    Our contract can create multiple simple storage contracts and can interact with functions of each contract

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -5,8 +5,21 @@ pragma solidity 0.8.18;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory{
-  SimpleStorage public simplestorage;
-  function createsimplestorage() public{
-    simplestorage = new SimpleStorage();
-  }
+    SimpleStorage[] public listofSimpleStorages;
+    function CreateSimpleStorage() public{
+        SimpleStorage mySimpleStorage = new SimpleStorage();
+        listofSimpleStorages.push(mySimpleStorage);
+    }
+    function sfStore(uint _simpleStorageIndex, uint num1) public{
+        SimpleStorage mySimpleStorage = listofSimpleStorages[_simpleStorageIndex];
+        mySimpleStorage.store(num1);
+    }
+    function sfView(uint _simpleStorageIndex) public view returns (uint){
+        SimpleStorage mySimpleStorage = listofSimpleStorages[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    }
 }
+
+
+
+
