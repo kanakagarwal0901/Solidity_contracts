c7828b1cc73feef7e66d2654e60a7298a6be4569
kanakagarwal0901 <kanakagarwal0901@gmail.com>
2026-09-13 01:56:10 +0530

    Fund me full complete except withdraw

--- FundMe.sol
+++ FundMe.sol
@@ -5,16 +5,26 @@ pragma solidity 0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
-    uint public minimumusd = 5;
+    uint public MinnimumUsd = 5e18;
+    address[] public SendersList;
+    mapping(address SendersAddress => uint Value) public SendersMapping;
     function fund() public payable{
-        require(msg.value>minimumusd,"Min transact value is 1Eth");
+        require(Conversion(msg.value) >= MinnimumUsd);
+        SendersList.push(msg.sender);
+        SendersMapping[msg.sender] = SendersMapping[msg.sender] + msg.value;
     }
-    function GetPrice() public{
-        }
-
-    function GetConversionRate() public{
+    function GetValue() public view returns(uint){
+        AggregatorV3Interface Feed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int price,,,) =  Feed.latestRoundData();
+        return uint(price * 1e10);
     }
     function GetVersion() public view returns(uint){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    } 
+        AggregatorV3Interface Feed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return Feed.version();
+    }
+    function Conversion(uint EthValue) public view returns(uint){
+        uint UnitPrice = GetValue();
+        uint usd =  (UnitPrice * EthValue)/1e18;
+        return usd;
+    }
 }
