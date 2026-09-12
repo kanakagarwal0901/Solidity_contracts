bf4501f7ff013d103981b0c5f95f203435fa9d49
kanakagarwal0901 <kanakagarwal0901@gmail.com>
2026-09-12 23:24:49 +0530

    AggregatorV3Interface for chainlink contract

--- FundMe.sol
+++ FundMe.sol
@@ -2,8 +2,35 @@
 
 pragma solidity 0.8.18;
 
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe{
+    uint public minimumusd = 5;
     function fund() public payable{
-        require(msg.value>1e18,"Min transact value is 1Eth");
+        require(msg.value>minimumusd,"Min transact value is 1Eth");
     }
+    function GetPrice() public{
+        }
+
+    function GetConversionRate() public{
+    }
+    function GetVersion() public view returns(uint){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    } 
 }
