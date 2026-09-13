534d18c6ea26177a30de2084a26de897cf873930
kanakagarwal0901 <kanakagarwal0901@gmail.com>
2026-09-13 22:41:43 +0530

    Internal changes

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -5,16 +5,16 @@ pragma solidity 0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter{
-  function GetValue() public view returns(uint){
+  function GetValue() internal view returns(uint){
         AggregatorV3Interface Feed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int price,,,) =  Feed.latestRoundData();
         return uint(price * 1e10);
     }
-    function GetVersion() public view returns(uint){
+    function GetVersion() internal view returns(uint){
         AggregatorV3Interface Feed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         return Feed.version();
     }
-    function Conversion(uint EthValue) public view returns(uint){
+    function Conversion(uint EthValue) internal view returns(uint){
         uint UnitPrice = GetValue();
         uint usd =  (UnitPrice * EthValue)/1e18;
         return usd;
