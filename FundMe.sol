e64d9da164866a5821f9fa80e687c72c0d28b18f
kanakagarwal0901 <kanakagarwal0901@gmail.com>
2026-09-13 22:41:19 +0530

    Withdraw function and own library

--- FundMe.sol
+++ FundMe.sol
@@ -2,29 +2,22 @@
 
 pragma solidity 0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint;
     uint public MinnimumUsd = 5e18;
     address[] public SendersList;
     mapping(address SendersAddress => uint Value) public SendersMapping;
     function fund() public payable{
-        require(Conversion(msg.value) >= MinnimumUsd);
+        require(msg.value.conversion() >= MinnimumUsd);
         SendersList.push(msg.sender);
         SendersMapping[msg.sender] = SendersMapping[msg.sender] + msg.value;
     }
-    function GetValue() public view returns(uint){
-        AggregatorV3Interface Feed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int price,,,) =  Feed.latestRoundData();
-        return uint(price * 1e10);
-    }
-    function GetVersion() public view returns(uint){
-        AggregatorV3Interface Feed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return Feed.version();
-    }
-    function Conversion(uint EthValue) public view returns(uint){
-        uint UnitPrice = GetValue();
-        uint usd =  (UnitPrice * EthValue)/1e18;
-        return usd;
+    function withdraw() public{
+        for(i = 0; i<SendersList.length; i++){
+            address SenderAddress = SendersList[i];
+            SendersMapping[SendersAddress] = 0;
+        }
     }
 }
